4ea8a93ee4a28ac6b63f5b3c3367ee1ca96c1571
WEBZaytsev <nikita@zaitsv.dev>
2024-10-27 20:27:40 +0300

    delete typegoose

--- src/models/Context.ts
+++ src/models/Context.ts
@@ -1,11 +1,10 @@
 import { Context as BaseContext } from 'grammy'
-import { DocumentType } from '@typegoose/typegoose'
 import { I18nContext } from '@grammyjs/i18n/dist/source'
 import { User } from '@/models/User'
 
 class Context extends BaseContext {
   readonly i18n!: I18nContext
-  dbuser!: DocumentType<User>
+  dbuser!: User
 
   replyWithLocalization: this['reply'] = (text, other, ...rest) => {
     text = this.i18n.t(text)
